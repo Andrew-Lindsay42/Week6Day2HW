245ac1f55793d939b2f9227f3ef565ef91a036af
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-05 18:10:30 +0000

    Added tests for Park attributes

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,14 +5,26 @@ const Dinosaur = require('../models/dinosaur.js');
 describe('Park', function() {
 
   beforeEach(function () {
+    const t_rex = new Dinosaur('T-Rex', 'carnivore', 50);
+    const stegosaurus = new Dinosaur('Stegosaurus', 'herbivore', 25);
+    const velociraptor = new Dinosaur('Velociraptor', 'carnivore', 35);
+    const park = new Park('Jurassic Park', 10, [t_rex, stegosaurus, velociraptor]);
+  });
 
-  })
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurassic Park');
+  });
 
-  it('should have a name');
+  it('should have a ticket price', function (){
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 10);
+  });
 
-  it('should have a ticket price');
-
-  it('should have a collection of dinosaurs');
+  it('should have a collection of dinosaurs', function (){
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [t_rex, stegosaurus, velociraptor])
+  });
 
   it('should be able to add a dinosaur to its collection');
 
