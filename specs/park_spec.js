308ecd50f485b3edc204460193761ad4dc96a701
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-05 18:47:05 +0000

    Added test for dino related Park functions

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -28,16 +28,38 @@ describe('Park', function() {
 
   it('should have a collection of dinosaurs', function (){
     const actual = park.dinosaurs;
-    assert.deepStrictEqual(actual, [t_rex, stegosaurus, velociraptor])
+    assert.deepStrictEqual(actual, [t_rex, stegosaurus, velociraptor]);
   });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to add a dinosaur to its collection', function (){
+    const new_dino = new Dinosaur('Brontosaurus', 'herbivore', 30);
+    park.addDino(new_dino);
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [t_rex, stegosaurus, velociraptor, new_dino]);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function (){
+    park.removeDino(t_rex);
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [stegosaurus, velociraptor]);
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors', function (){
+    const actual = park.coolestDino();
+    assert.strictEqual(actual, t_rex);
+  });
 
-  it('should be able to find all dinosaurs of a particular species');
+  it('should be able to find all dinosaurs of a particular species', function (){
+    const velociraptor2 = new Dinosaur('Velociraptor', 'carnivore', 30);
+    const velociraptor3 = new Dinosaur('Velociraptor', 'carnivore', 33);
+    const velociraptor4 = new Dinosaur('Velociraptor', 'carnivore', 31);
+    park.addDino(velociraptor2);
+    park.addDino(velociraptor3);
+    park.addDino(velociraptor4);
+
+    const actual = park.findDinos('Velocripator');
+    assert.deepStrictEqual(actual, [velociraptor, velociraptor2, velociraptor3, velociraptor4])
+  });
 
   it('should be able to calculate the total number of visitors per day');
 
